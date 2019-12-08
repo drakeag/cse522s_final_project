f026120c9f5ca1d89401e37d31ca30dde1140af4
Alex Drake <adrake2@gmail.com>
2019-12-08 09:05:08 -0600

    Add final test results

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,50 @@
 #!/bin/bash
 
-RESULTS_FILE=results.txt
+if [ "$1" != "" ]; then
+	RESULTS_FILE=$1
+else
+	RESULTS_FILE=results.txt
+fi
 PERF_EVENTS=cache-references,cache-misses,page-faults,dTLB-load-misses,context-switches
 TEST_NUM=1
 SYS_MMAP=0
 DRIVER_MMAP=1
+
+load_mmu_module() {
+	sudo insmod ../mmu_module.ko
+}
+
+unload_mmu_module() {
+	sudo rmmod mmu_module
+}
+
+do_test() {
+	# $1 TEST_NUM
+	# $2 CMD
+	echo "Test $1"
+	echo "Test $1" >> $RESULTS_FILE
+	# Write the command to the results file
+	echo $2 >> $RESULTS_FILE
+	
+	if [ "$3" == $DRIVER_MMAP ]; then
+		# Load the module
+		load_mmu_module
+	fi
+	
+	# Run the command and write the output to the results file
+	# 2> is the output from the program, 1> is the output from perf
+	$2 2>>$RESULTS_FILE 1>>$RESULTS_FILE
+	echo >> $RESULTS_FILE
+	
+	if [ "$3" == $DRIVER_MMAP ]; then
+		# Unload the module
+		unload_mmu_module
+	fi
+}
+
+#echo Compiling module...
+#make clean
+#make
 
 echo Compiling test programs...
 cd tests
@@ -15,185 +55,31 @@ echo Running tests...
 echo Test Results > $RESULTS_FILE
 echo >> $RESULTS_FILE
 
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./simple_sequential 4096 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
-((++TEST_NUM))
-echo >> $RESULTS_FILE
 
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./simple_sequential 65536 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
-((++TEST_NUM))
-echo >> $RESULTS_FILE
 
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./simple_sequential 1048576 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
+# 23
+do_test $TEST_NUM "sudo ./perf stat -e ${PERF_EVENTS} ./random_access 4096 1 ${SYS_MMAP}"
 ((++TEST_NUM))
-echo >> $RESULTS_FILE
 
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./simple_sequential 16777216 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
+# 24
+do_test $TEST_NUM "sudo ./perf stat -e ${PERF_EVENTS} ./random_access 4096 1 ${DRIVER_MMAP}" $DRIVER_MMAP
 ((++TEST_NUM))
-echo >> $RESULTS_FILE
 
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./random_access 2 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
+# 25
+do_test $TEST_NUM "sudo ./perf stat -e ${PERF_EVENTS} ./random_access 65536 16 ${SYS_MMAP}" 
 ((++TEST_NUM))
-echo >> $RESULTS_FILE
 
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./random_access 4 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
+# 26
+do_test $TEST_NUM "sudo ./perf stat -e ${PERF_EVENTS} ./random_access 65536 16 ${DRIVER_MMAP}" $DRIVER_MMAP
 ((++TEST_NUM))
-echo >> $RESULTS_FILE
 
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./random_access 8 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
+# 27
+do_test $TEST_NUM "sudo ./perf stat -e ${PERF_EVENTS} ./random_access 1048576 256 ${SYS_MMAP}"
 ((++TEST_NUM))
-echo >> $RESULTS_FILE
 
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./random_access 16 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
+# 28
+do_test $TEST_NUM "sudo ./perf stat -e ${PERF_EVENTS} ./random_access 1048576 256 ${DRIVER_MMAP}" $DRIVER_MMAP
 ((++TEST_NUM))
-echo >> $RESULTS_FILE
-
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./dense_mm 64 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
-((++TEST_NUM))
-echo >> $RESULTS_FILE
-
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./dense_mm 512 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
-((++TEST_NUM))
-echo >> $RESULTS_FILE
-
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./dense_mm 1024 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
-((++TEST_NUM))
-echo >> $RESULTS_FILE
-
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./context_switching 1024 1 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
-((++TEST_NUM))
-echo >> $RESULTS_FILE
-
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./context_switching 4096 1 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
-((++TEST_NUM))
-echo >> $RESULTS_FILE
-
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./context_switching 32768 1024 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
-((++TEST_NUM))
-echo >> $RESULTS_FILE
-
-TEST="Test ${TEST_NUM}"
-echo $TEST
-echo $TEST >> $RESULTS_FILE
-CMD="sudo ./perf stat -e ${PERF_EVENTS} ./context_switching 16777216 32768 ${SYS_MMAP}"
-# Write the command to the results file
-echo $CMD >> $RESULTS_FILE
-# Run the command and write the output to the results file
-# 2> is the output from the program, 1> is the output from perf
-$CMD 2>>$RESULTS_FILE 1>>$RESULTS_FILE
-((++TEST_NUM))
-echo >> $RESULTS_FILE
 
 echo Tests complete
 
---
+++ unload_module.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo rmmod mmu_module
